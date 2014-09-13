11fd352a660210cc81e6788da6eb3e4010690f67
Tadas Antanavicius <tadas412@gmail.com>
2014-09-13 00:28:09 -0400

    finished groupmodel

---
+++ Classes/GroupsModel.swift
@@ -0,0 +1,26 @@
+//
+//  GroupsModel.swift
+//  Conversify
+//
+//  Created by Tadas Antanavicius on 9/12/14.
+//  Copyright (c) 2014 Zach Costa. All rights reserved.
+//
+
+class GroupsModel: Model {
+    
+    /// Returns a dictionary with all the groups that a user is part of
+    /// Returns an array of groups, <String, Any>
+    /// Key, Value: groupID, String | groupName, String | isAdmin, Boolean
+    func getUserGroups(userID: String) -> [Dictionary<String, Any>]? {
+        var groups: [Dictionary<String, Any>]?
+        // TODO request user's groups
+        return groups
+    }
+    
+    /// Returns if user was successfully removed from the group
+    func sendLeaveGroup(userID: String, groupID: String) -> Bool {
+        // TODO post leaving group
+        return false
+    }
+    
+}
