5990ab6ae705933ca182553d34d4d1257d6c0a24
Tadas Antanavicius <tadas412@gmail.com>
2014-09-13 10:06:12 -0400

    Model update

--- Classes/Model.swift
+++ Classes/Model.swift
@@ -6,14 +6,59 @@
 //  Copyright (c) 2014 Zach Costa. All rights reserved.
 //
 
-/// Parent model for Conversify. Holds some of the most generic model functions.
-class Model {
+import Foundation
+
+/// Parent model for Conversify, holds the high level functions.
+class Model: LiveWebsocketProtocol, PassiveWebsocketProtocol {
+
+    // Note: Websocket needs a delegate to listen to requests, and my class structure
+    // requires that delegate to have this model act as a delegate in return
+    var liveWebsocket = Websocket(url: NSURL.URLWithString("ws://localhost:8080"))
+    var passiveWebsocket = Websocket(url: NSURL.URLWithString("ws://localhost:8080"))
+    var liveWebsocketDelegate, passiveWebsocketDelegate : LiveWebsocketDelegate?
     
-    var databasePath : String = ""
-    
-    func Model (path: String) {
-        databasePath = path;
+    func Model() {
+        liveWebsocketDelegate = LiveWebsocketDelegate(listener: self)
+        liveWebsocket.delegate = liveWebsocketDelegate
+        liveWebsocket.connect()
     }
+    
+    // Live Websocket
+    
+    func onLiveWebsocketReceiveData(data: NSData) {
+        println("App received data in live socket")
+        // Call VC delegate that there's new data
+    }
+    
+    func onLiveWebsocketReceiveMessage(text: String) {
+        println("App received message '\(text)' in live socket")
+        // Call VC delegate that there's new data
+    }
+    
+    func sendLiveWebsocketMessage(text: String) {
+        liveWebsocket.writeString(text)
+    }
+    
+    func sendLiveWebsocketData(data: NSData) {
+        liveWebsocket.writeData(data)
+    }
+    
+    // Passive Websocket
+    
+    // Groups
+    
+    // CreateGroup
+    
+    // Conversations
+    
+    // GroupSettings
+    
+    // Home
+    // TODO on load, initialize liveMessageSocket
+    
+    // Messages
+    
+    // ComposeMessages
     
 }
 
