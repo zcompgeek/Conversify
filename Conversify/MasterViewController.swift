61c4ecb54756a5d7b47d8891f3f4b131d7eb581d
zcompgeek <zach.l.costa@gmail.com>
2014-09-13 23:45:01 -0400

    json error

--- Conversify/MasterViewController.swift
+++ Conversify/MasterViewController.swift
@@ -100,7 +100,7 @@ class MasterViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
-        return true
+        return false
     }
 
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
